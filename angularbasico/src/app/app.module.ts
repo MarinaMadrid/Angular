33d5ea121f921763f24dca89b13372ff778b051c
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2021-09-24 16:05:37 +0200

    add angular basico

---
+++ angularbasico/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+//Rutas
+import { APP_ROUTING } from './app.routes';
+
+//Servicios
+import { HeroesService } from './services/heroes.service';
+
+//Componentes
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { HeroeTarjetaComponent } from './components/shared/heroe-tarjeta/heroe-tarjeta.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    HeroeTarjetaComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    APP_ROUTING
+  ],
+  providers: [
+    HeroesService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
